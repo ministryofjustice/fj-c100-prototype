0af7df8089a7f7fb604a6429454912c19401b912
Alex Robinson <github@alex.fu2k.org>
2017-11-13 23:37:17 +0000

    Create basic JTBD playback for order types

---
+++ app/templates/jtbd_playback/jtbd_playback.js
@@ -0,0 +1,30 @@
+const PlaybackController = (req, res) => {
+  return (routeInstance, methods) => {
+    const controller = new Promise(resolve => {
+
+      const autofields = routeInstance.autofields
+      const optionsBundle = []
+      const orderTypes = methods.getBlockProp('collection:order-types', 'items', [])
+      orderTypes.forEach(orderCollection => {
+        const options = methods.getBlockProp(orderCollection, 'items', [])
+        const selectedOptions = options.filter(option => autofields[option] === 'yes')
+        if (selectedOptions.length) {
+          optionsBundle.push({
+            collection: orderCollection,
+            selected: selectedOptions
+          })
+        }
+      })
+
+      // routeInstance.blocks = []
+      routeInstance.playbackValues = optionsBundle
+
+      resolve(routeInstance)
+    })
+    return controller
+  }
+}
+
+module.exports = PlaybackController
+
+//   "controller": "app/templates/message/message",
