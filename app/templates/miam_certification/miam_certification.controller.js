890ac64483648ff26c6ea5d0c5bd4865fe833272
Alex Robinson <github@alex.fu2k.org>
2017-11-28 18:24:03 +0000

    Implment adjusted MIAM attendance journey

--- app/templates/miam_certification/miam_certification.controller.js
+++ app/templates/miam_certification/miam_certification.controller.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const mediatorAPI = require('./mediator-api')
-const DateController = require('pflr-express-kit/app/components/Date/Date.controller')
 
 const MIAMCertificationController = (req, res) => {
   return (routeInstance, methods) => {
@@ -32,28 +31,6 @@ const MIAMCertificationController = (req, res) => {
         const codeValid = mediatorAPI.isValid(fmcURN)
         if (!codeValid) {
           setError('miam_certification_code', 'code-invalid', 'Not a valid MIAM attendance code')
-        }
-      }
-      if (!routeInstance.errors || !routeInstance.errors.miam_certification_date) {
-        const setDateError = (name, message) => {
-          setError('miam_certification_date', name, message, ['day', 'month', 'year'])
-        }
-        const dates = {
-          day: autofields['miam_certification_date.day'],
-          month: autofields['miam_certification_date.month'],
-          year: autofields['miam_certification_date.year'],
-        }
-        const now = new Date()
-        const attendanceDate = new Date(`${dates.year}/${dates.month}/${dates.day}`)
-        if (attendanceDate > now) {
-          setDateError('code-future', 'Date given for MIAM attendance is in the future')
-        } else {
-          const allowedMonths = 4
-          const allowedElapsed = allowedMonths * 31 * 24 * 60 * 60 * 1000
-          const elapsed = now.getTime() - attendanceDate.getTime()
-          if (elapsed > allowedElapsed) {
-            setDateError('code-expired', 'MIAM attendance took place over 4 months ago')
-          }
         }
       }
 
