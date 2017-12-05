7383129ec977f5691e05127a12f577b666505c6e
Alex Robinson <github@alex.fu2k.org>
2017-12-05 20:29:15 +0000

    Remove first child instance from list of children

--- app/templates/relationship-same/relationship-same.controller.js
+++ app/templates/relationship-same/relationship-same.controller.js
@@ -4,7 +4,7 @@ const RelationshipController = (req, res) => {
 
       const childrenNames = []
       const children = methods.getValue('children', 0)
-      for (let index = 0; index < children; index++) {
+      for (let index = 1; index < children; index++) {
         childrenNames.push(methods.getValue(`child_used-name_${index + 1}`))
       }
       routeInstance.autofields.childrenNames = childrenNames
