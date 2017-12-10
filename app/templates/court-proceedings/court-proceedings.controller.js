d4ce42ef2c9fa25f5ad2a6f27c71a5bdf781f82b
Alex Robinson <github@alex.fu2k.org>
2017-12-10 21:56:04 +0000

    Add ability to select children for court case

---
+++ app/templates/court-proceedings/court-proceedings.controller.js
@@ -0,0 +1,34 @@
+const ProceedingsController = (req, res) => {
+  return (routeInstance, methods) => {
+    const controller = new Promise(resolve => {
+      let proceedingsOptions = []
+
+      const childrenNames = methods.getMultipleValues('children', 'child_used-name_')
+      const otherChildrenNames = methods.getMultipleValues('other-children', 'other-children_full-name_')
+      if (childrenNames.length + otherChildrenNames.length > 1) {
+        const childrenOptions = childrenNames.map((name, index) => {
+          return {
+            name: `children_${index + 1}`,
+            xvalue: 'yes',
+            label: name
+          }
+        })
+        const otherChildrenOptions = otherChildrenNames.map((name, index) => {
+          return {
+            name: `other_children_${index + 1}`,
+            xvalue: 'yes',
+            label: name
+          }
+        })
+        proceedingsOptions = [].concat(childrenOptions, otherChildrenOptions)
+        routeInstance.proceedingsOptions = proceedingsOptions
+        routeInstance.proceedingsName = 'proceedings-children-options'
+      }
+
+      resolve(routeInstance)
+    })
+    return controller
+  }
+}
+
+module.exports = ProceedingsController
