e9940cce19d1cace40b88c6aab1ae818707113d2
Alex Robinson <github@alex.fu2k.org>
2017-12-11 19:11:30 +0000

    Allow other party to be added from children residence other option

--- app/templates/children-residence/children-residence.controller.js
+++ app/templates/children-residence/children-residence.controller.js
@@ -5,18 +5,29 @@ const ResidenceController = (req, res) => {
       const autofields = routeInstance.autofields
       const applicants = methods.getValue('applicants', 1)
       const respondents = methods.getValue('respondents', 1)
-      let other_parties = methods.getValue('other-parties', 0)
+      let otherParties = methods.getValue('other-parties', 0)
       if (methods.getValue('other-parties-required') !== 'yes') {
-        other_parties = 0
+        otherParties = 0
       }
+      let otherPartyChecked
+
+      if (req.method === 'GET') {
+        const residenceOtherDetails = methods.getValue(`${routeInstance._prefix}residence-other-details`)
+        if (residenceOtherDetails) {
+          otherPartyChecked = otherParties
+          delete routeInstance.autofields[`${routeInstance._prefix}residence-other`]
+          delete routeInstance.autofields[`${routeInstance._prefix}residence-other-details`]
+        }
+      }
+
       let options = []
-      const getOptions = (type, counter, options) => {
+      const getOptions = (type, counter, options, checkIndex) => {
         for (let index = 1; index <= counter; index++) {
           const label = methods.getValue(`${type}_full-name_${index}`)
           if (label) {
             const name = `${type}_${index}`
             const prefixedName = routeInstance._prefix + name
-            const checked = !!methods.getValue(prefixedName)
+            const checked = !!methods.getValue(prefixedName) || (checkIndex ? index === otherPartyChecked : false)
             options.push({
               name,
               label,
@@ -28,13 +39,41 @@ const ResidenceController = (req, res) => {
       }
       options = getOptions('applicants', applicants, options)
       options = getOptions('respondents', respondents, options)
-      options = getOptions('other-parties', other_parties, options)
-      options.push('option:other')
+      options = getOptions('other-parties', otherParties, options, true)
+      options.push('residence-other')
       routeInstance.residenceOptions = options
       routeInstance.residenceName = 'child-residence'
+      routeInstance.blocks.pop()
 
       if (req.method === 'POST') {
-        if (!options.filter(option => option.checked).length) {
+        const otherArg = Object.keys(req.body).filter(key => key.endsWith('residence-other'))[0]
+        const otherArgDetails = Object.keys(req.body).filter(key => key.endsWith('residence-other-details'))[0] || ''
+        if (otherArg) {
+          if (otherArgDetails) {
+            const otherPartyCount = otherParties + 1
+            const otherPartyName = `other-parties_full-name_${otherPartyCount}`
+            routeInstance.autofields['other-parties'] = otherPartyCount
+            routeInstance.autofields['other-parties-required'] = 'yes'
+            routeInstance.autofields[otherPartyName] = req.body[otherArgDetails]
+            // methods.setFieldValue({
+            //   'other-parties': otherPartyCount,
+            //   'other-parties-required': 'yes',
+            //   [otherPartyName]: req.body[otherArgDetails]
+            // })
+            res.redirect('/other-party/details/' + otherPartyCount)
+            return
+          } else {
+            const prefixedErrorName = routeInstance._prefix + 'residence-other-details'
+            routeInstance.errors = routeInstance.errors || {}
+            routeInstance.errors[prefixedErrorName] = {
+              property: 'instance',
+              message: 'This field is required',
+              name: 'required'
+            }
+
+          }
+
+        } else if (!options.filter(option => option.checked).length) {
           const prefixedErrorName = routeInstance._prefix + routeInstance.residenceName
           routeInstance.errors = routeInstance.errors || {}
           routeInstance.errors[prefixedErrorName] = {
