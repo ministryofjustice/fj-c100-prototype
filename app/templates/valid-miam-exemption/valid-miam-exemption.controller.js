792753b2f036b34cc9d17c2141c7bd92bfa99c5e
Alex Robinson <github@alex.fu2k.org>
2018-01-09 23:20:33 +0000

    Add ultra-simple playback for exemptions

---
+++ app/templates/valid-miam-exemption/valid-miam-exemption.controller.js
@@ -0,0 +1,17 @@
+const ValidExemptionController = (req, res) => {
+  return (routeInstance, methods) => {
+    const controller = new Promise(resolve => {
+
+      const autofields = routeInstance.autofields
+
+      routeInstance.autofields.validReasons = Object.keys(autofields)
+                                                .filter(key => key.startsWith('auto-exemption-claimed'))
+                                                .map(key => methods.getBlockProp(key, 'label'))
+
+      resolve(routeInstance)
+    })
+    return controller
+  }
+}
+
+module.exports = ValidExemptionController
