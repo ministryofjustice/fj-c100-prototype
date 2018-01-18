d61f81b63843a05634888934a5f061444766f50a
Alex Robinson <github@alex.fu2k.org>
2018-01-18 13:45:44 +0000

    Rewire MIAM exemptions to accomodate MIAM attendance loop hole

--- app/templates/valid-miam-exemption/valid-miam-exemption.controller.js
+++ app/templates/valid-miam-exemption/valid-miam-exemption.controller.js
@@ -4,9 +4,17 @@ const ValidExemptionController = (req, res) => {
 
       const autofields = routeInstance.autofields
 
-      routeInstance.autofields.validReasons = Object.keys(autofields)
+      const validReasons = Object.keys(autofields)
                                                 .filter(key => key.match(/^(auto|misc)-exemption-claimed/))
                                                 .map(key => methods.getBlockProp(key, 'label'))
+      if (!validReasons.length) {
+        if (autofields.miam_attended === 'yes' && autofields.miam_certification === 'no') {
+          const attendedKey = 'exemption-claimed_adr_previous-attendance'
+          validReasons.push(methods.getBlockProp(attendedKey, 'label'))
+        }
+      }
+
+      routeInstance.autofields.validReasons = validReasons
 
       resolve(routeInstance)
     })
