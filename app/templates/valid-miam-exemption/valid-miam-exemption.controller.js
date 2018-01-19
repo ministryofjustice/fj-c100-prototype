6b3866fafdbf1dbbad0c55c66d28719d76d23501
Alex Robinson <github@alex.fu2k.org>
2018-01-19 11:09:34 +0000

    Add auto-exemptions for laa involvement

--- app/templates/valid-miam-exemption/valid-miam-exemption.controller.js
+++ app/templates/valid-miam-exemption/valid-miam-exemption.controller.js
@@ -4,16 +4,31 @@ const ValidExemptionController = (req, res) => {
 
       const autofields = routeInstance.autofields
 
-      const validReasons = Object.keys(autofields)
-                                                .filter(key => key.match(/^(auto|misc)-exemption-claimed/))
-      if (!validReasons.length) {
-        if (autofields.miam_attended === 'yes' && autofields.miam_certification === 'no') {
-          const attendedKey = 'exemption-claimed_adr_previous-attendance'
-          validReasons.push(attendedKey)
-        }
+      const mappedFields = {
+        'children_known-to-authorities': 'auto-exemption-claimed_local-authority-involvement_section47',
+        'children_child-protection-plan': 'auto-exemption-claimed_local-authority-involvement_protection-plan'
       }
 
-      routeInstance.validReasons = validReasons
+      Object.keys(mappedFields).forEach(answer => {
+        const mappedAnswer = mappedFields[answer]
+        if (autofields[answer] === 'yes') {
+          autofields[mappedAnswer] = 'yes'
+        } else {
+          delete autofields[mappedAnswer]
+        }
+      })
+
+      const validReasons = Object.keys(autofields)
+                                                .filter(key => key.match(/^(auto|misc)-exemption-claimed/))
+
+      const attendedKey = 'exemption-claimed_adr_previous-attendance'
+      if (!validReasons.length) {
+        if (autofields.miam_attended === 'yes' && autofields.miam_certification === 'no') {
+          routeInstance.autofields[attendedKey] = 'yes'
+        }
+      } else {
+        delete routeInstance.autofields[attendedKey]
+      }
 
       resolve(routeInstance)
     })
